df1cee96ba623c350323f3018b0badc75ba84044
snehaljoshi0904 <snehaljoshi0904@gmail.com>
2021-03-03 14:18:08 +0000

    Initail Commit

---
+++ pkg/consts/consts.go
@@ -0,0 +1,14 @@
+package consts
+
+const(
+
+	//AppName represent an appName .
+	AppName = "goservice"
+	//Usage description of app
+	Usage =  "A command line application "
+	//Author author
+	Author = "golangapps.com"
+	//Version app version
+	Version = "1.0.0"
+
+)
